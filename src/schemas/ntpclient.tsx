474c8014923955223b7ea25700e383b44665d3e0
Joshua Lee <24319042+bclswl0827@users.noreply.github.com>
2025-09-28 11:46:50 +0000

    feat: Support importing NTP server groups

--- src/schemas/ntpclient.tsx
+++ src/schemas/ntpclient.tsx
@@ -11,6 +11,140 @@ const defaultConfig = {
     retry: 5
 };
 
+const defaultNtpGroup = [
+    {
+        tag: 'university-provider',
+        servers: [
+            { address: 'time.mit.edu', port: '123' },
+            { address: 'ntp1.lsu.edu', port: '123' },
+            { address: 'ntp2.lsu.edu', port: '123' },
+            { address: 'ntp3.lsu.edu', port: '123' },
+            { address: 'ntp1.tamu.edu', port: '123' },
+            { address: 'ntp2.tamu.edu', port: '123' },
+            { address: 'ntp3.tamu.edu', port: '123' },
+            { address: 'ntp1.cs.princeton.edu', port: '123' },
+            { address: 'ntp2.cs.princeton.edu', port: '123' },
+            { address: 'ntppub.tamu.edu', port: '123' },
+            { address: 'ntp.illinois.edu', port: '123' },
+            { address: 'ntp1.tohoku.ac.jp', port: '123' },
+            { address: 'ntp2.tohoku.ac.jp', port: '123' },
+            { address: 'ntp1.noc.titech.ac.jp', port: '123' },
+            { address: 'gpsntp.miz.nao.ac.jp', port: '123' },
+            { address: 'ntp.nc.u-tokyo.ac.jp', port: '123' },
+            { address: 'clock.cit.nihon-u.ac.jp', port: '123' },
+            { address: 'ntp.osakafu-u.ac.jp', port: '123' },
+            { address: 'ntp.kuins.kyoto-u.ac.jp', port: '123' },
+            { address: 'ntp.tut.ac.jp', port: '123' },
+            { address: 'ntp.netlab.is.tsukuba.ac.jp', port: '123' },
+            { address: 'ntp.nagaokaut.ac.jp', port: '123' },
+            { address: 'ntp.hiroshima-u.ac.jp', port: '123' },
+            { address: 'ntp.kj.yamagata-u.ac.jp', port: '123' },
+            { address: 'ntp0.cam.ac.uk', port: '123' },
+            { address: 'ntp1.cam.ac.uk', port: '123' },
+            { address: 'ntp2.cam.ac.uk', port: '123' },
+            { address: 'ntp3.cam.ac.uk', port: '123' },
+            { address: 'ntp1.uni-ulm.de', port: '123' },
+            { address: 'ntp2.uni-ulm.de', port: '123' },
+            { address: 'ntp3.uni-ulm.de', port: '123' },
+            { address: 'ntp.ntu.edu.tw', port: '123' },
+            { address: 'ntp2.ntu.edu.tw', port: '123' },
+            { address: 'time.upd.edu.ph:123', port: '123' }
+        ]
+    },
+    {
+        tag: 'authority-provider',
+        servers: [
+            { address: 'ntp-a2.nict.go.jp', port: '123' },
+            { address: 'ntp-a3.nict.go.jp', port: '123' },
+            { address: 'ntp-b2.nict.go.jp', port: '123' },
+            { address: 'ntp-b3.nict.go.jp', port: '123' },
+            { address: 'ntp-k1.nict.jp', port: '123' },
+            { address: 'ptbtime1.ptb.de', port: '123' },
+            { address: 'ptbtime2.ptb.de', port: '123' },
+            { address: 'ptbtime3.ptb.de', port: '123' },
+            { address: 'stdtime.gov.hk', port: '123' },
+            { address: 'time1.chu.nrc.ca', port: '123' },
+            { address: 'time2.chu.nrc.ca', port: '123' },
+            { address: 'ntp.metas.ch', port: '123' },
+            { address: 'hora.roa.es', port: '123' },
+            { address: 'minuto.roa.es', port: '123' }
+        ]
+    },
+    {
+        tag: 'apple-inc',
+        servers: [
+            { address: 'krseo1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'dkblp1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'dkblp1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'frcch1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'frcch1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'krsel6-ntp-001.aaplimg.com', port: '123' },
+            { address: 'krsel6-ntp-002.aaplimg.com', port: '123' },
+            { address: 'nlams2-ntp-001.aaplimg.com', port: '123' },
+            { address: 'nlams2-ntp-002.aaplimg.com', port: '123' },
+            { address: 'twtpe2-ntp-001.aaplimg.com', port: '123' },
+            { address: 'twtpe2-ntp-002.aaplimg.com', port: '123' },
+            { address: 'usmes1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'ntp.euro.apple.com', port: '123' },
+            { address: 'uklon5-ntp-001.aaplimg.com', port: '123' },
+            { address: 'zaisd1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'zaisd1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'jptyo5-ntp-001.aaplimg.com', port: '123' },
+            { address: 'jptyo5-ntp-004.aaplimg.com', port: '123' },
+            { address: 'defra1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'sesto4-ntp-002.aaplimg.com', port: '123' },
+            { address: 'sesto4-ntp-001.aaplimg.com', port: '123' },
+            { address: 'defra1-ntp-004.aaplimg.com', port: '123' },
+            { address: 'usatl4-ntp-001.aaplimg.com', port: '123' },
+            { address: 'usatl4-ntp-002.aaplimg.com', port: '123' },
+            { address: 'uschi5-ntp-002.aaplimg.com', port: '123' },
+            { address: 'uschi5-ntp-003.aaplimg.com', port: '123' },
+            { address: 'usscz2-ntp-002.aaplimg.com', port: '123' },
+            { address: 'ussjc2-ntp-002.aaplimg.com', port: '123' },
+            { address: 'usscz2-ntp-001.aaplimg.com', port: '123' },
+            { address: 'ussjc2-ntp-001.aaplimg.com', port: '123' },
+            { address: 'jptyo5-ntp-003.aaplimg.com', port: '123' },
+            { address: 'jptyo5-ntp-002.aaplimg.com', port: '123' },
+            { address: 'hkhkg1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'hkhkg1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'hkhkg1-ntp-003.aaplimg.com', port: '123' },
+            { address: 'defra1-ntp-003.aaplimg.com', port: '123' },
+            { address: 'defra1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'uklon5-ntp-004.aaplimg.com', port: '123' },
+            { address: 'usmes1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'usmia1-ntp-001.aaplimg.com', port: '123' },
+            { address: 'usmia1-ntp-002.aaplimg.com', port: '123' },
+            { address: 'usnyc3-ntp-003.aaplimg.com', port: '123' },
+            { address: 'usnyc3-ntp-001.aaplimg.com', port: '123' },
+            { address: 'usnyc3-ntp-004.aaplimg.com', port: '123' },
+            { address: 'usnyc3-ntp-002.aaplimg.com', port: '123' },
+            { address: 'brsao4-ntp-001.aaplimg.com', port: '123' },
+            { address: 'brsao4-ntp-002.aaplimg.com', port: '123' },
+            { address: 'usqas2-ntp-001.aaplimg.com', port: '123' },
+            { address: 'usqas2-ntp-002.aaplimg.com', port: '123' },
+            { address: 'uschi5-ntp-001.aaplimg.com', port: '123' },
+            { address: 'ussea4-ntp-001.aaplimg.com', port: '123' },
+            { address: 'ussea4-ntp-002.aaplimg.com', port: '123' },
+            { address: 'uklon5-ntp-003.aaplimg.com', port: '123' },
+            { address: 'uklon5-ntp-002.aaplimg.com', port: '123' },
+            { address: 'ausyd2-ntp-001.aaplimg.com', port: '123' },
+            { address: 'ausyd2-ntp-002.aaplimg.com', port: '123' },
+            { address: 'sgsin3-ntp-001.aaplimg.com', port: '123' },
+            { address: 'sgsin3-ntp-002.aaplimg.com', port: '123' },
+            { address: 'hkhkg1-ntp-004.aaplimg.com', port: '123' }
+        ]
+    },
+    {
+        tag: 'google-inc',
+        servers: [
+            { address: 'time1.google.com', port: '123' },
+            { address: 'time2.google.com', port: '123' },
+            { address: 'time3.google.com', port: '123' },
+            { address: 'time4.google.com', port: '123' }
+        ]
+    }
+];
+
 export const NtpClient = ({ onCreate, onUpdate }: ISchema) => {
     const initialized = useRef(false);
     const [currentConfig, setCurrentConfig] = useState({ ...defaultConfig });
@@ -21,22 +155,7 @@ export const NtpClient = ({ onCreate, onUpdate }: ISchema) => {
         }
     }, [currentConfig, onCreate]);
 
-    const [ntpAddrs, setNtpAddrs] = useState([
-        { address: 'ntp-a2.nict.go.jp', port: '123' },
-        { address: 'ntp-a3.nict.go.jp', port: '123' },
-        { address: 'ntp-b2.nict.go.jp', port: '123' },
-        { address: 'ntp-b3.nict.go.jp', port: '123' },
-        { address: 'ntp-k1.nict.jp', port: '123' },
-        { address: 'ptbtime1.ptb.de', port: '123' },
-        { address: 'ptbtime2.ptb.de', port: '123' },
-        { address: 'ptbtime3.ptb.de', port: '123' },
-        { address: 'stdtime.gov.hk', port: '123' },
-        { address: 'time1.chu.nrc.ca', port: '123' },
-        { address: 'time2.chu.nrc.ca', port: '123' },
-        { address: 'ntp.metas.ch', port: '123' },
-        { address: 'hora.roa.es', port: '123' },
-        { address: 'minuto.roa.es', port: '123' }
-    ]);
+    const [ntpAddrs, setNtpAddrs] = useState([...defaultNtpGroup[0].servers]);
 
     const handleUpdateNtpAddr = (index: number, value: string) => {
         if (ntpAddrs.filter((item) => item.address.length > 0).length === 1 && value.length === 0) {
@@ -65,11 +184,15 @@ export const NtpClient = ({ onCreate, onUpdate }: ISchema) => {
     };
 
     const handleRemoveNtpItem = (index: number) => {
-        if (ntpAddrs.length > 1) {
+        if (ntpAddrs[index].address.length === 0) {
             setNtpAddrs(ntpAddrs.filter((_, i) => i !== index));
-        } else {
-            sendUserAlert('At least one NTP server is required', true);
+            return;
         }
+        if (ntpAddrs.filter((i) => i.address.length > 0).length === 1) {
+            sendUserAlert('At least one NTP server is required', true);
+            return;
+        }
+        setNtpAddrs(ntpAddrs.filter((_, i) => i !== index));
     };
 
     const updatePool = useCallback(() => {
@@ -109,14 +232,27 @@ export const NtpClient = ({ onCreate, onUpdate }: ISchema) => {
     return (
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box space-y-2 border p-4">
             <div className="flex flex-col space-y-2">
-                {ntpAddrs.map((addr, index) => (
+                <div className="flex flex-wrap items-center gap-2">
+                    {defaultNtpGroup.map(({ servers, tag }, index) => (
+                        <button
+                            className="btn btn-sm text-gray-700"
+                            onClick={() => {
+                                setNtpAddrs(servers);
+                            }}
+                            key={index}
+                        >
+                            {tag}
+                        </button>
+                    ))}
+                </div>
+                {ntpAddrs.map(({ address, port }, index) => (
                     <div className="flex items-center justify-center space-x-2" key={index}>
                         <div className="w-1/2 space-y-2 lg:w-4/6">
                             <label className="label">Host {index + 1}</label>
                             <input
                                 type="text"
                                 className="input w-full"
-                                value={addr.address}
+                                value={address}
                                 placeholder='e.g. "ntp-a2.nict.go.jp"'
                                 onChange={({ target }) => handleUpdateNtpAddr(index, target.value)}
                             />
@@ -126,14 +262,14 @@ export const NtpClient = ({ onCreate, onUpdate }: ISchema) => {
                             <input
                                 type="number"
                                 className="input w-full"
-                                value={addr.port}
+                                value={port}
                                 onChange={({ target }) =>
                                     handleUpdateNtpPort(index, Number(target.value))
                                 }
                             />
                         </div>
                         <button
-                            className="btn btn-circle btn-sm mt-6 bg-red-400 hover:bg-red-500"
+                            className="btn btn-circle btn-xs mt-6.5 bg-red-400 hover:bg-red-500"
                             onClick={() => handleRemoveNtpItem(index)}
                         >
                             <Icon className="text-white" path={mdiMinus} size={0.8} />
